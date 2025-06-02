c4ab4d9b11dad74bbfc90e0fdd86a96e52a7a564
Upayan M <upayanm3@gmail.com>
2025-06-02 18:31:59 +0530

    fix: ensure baseURL falls back to default API URL if environment variable is not set

--- app/src/utils/api.ts
+++ app/src/utils/api.ts
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const baseURL = process.env.NEXT_PUBLIC_API_URL;
+const baseURL =
+  process.env.NEXT_PUBLIC_API_URL || "https://api.kargo.upayan.dev";
 
 const instance = axios.create({
   baseURL,
